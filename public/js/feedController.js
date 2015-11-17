1c29d717f9a3e0cfdd36bb7020da4b5a86ebe734
Avi Samloff <avi.samloff@gmail.com>
2015-11-17 15:53:24 -0800

    (feature) a whole bunch of shit

--- public/js/feedController.js
+++ public/js/feedController.js
@@ -1,4 +1,4 @@
-angular.module('feed', [])
+angular.module('tastebuds', [])
 
 .controller('feedController', function($scope, $http) {
   var postPicture = $http({
--- public/js/landingController.js
+++ public/js/landingController.js
@@ -1,7 +1,9 @@
-tastebuds.controller('landingController', function($scope) {
+angular.module('tastebuds')
+
+.controller('landingController', function($scope) {
   $scope.message = 'lol the landingController works.';
-  $scope.profile = {
-    username: users.username,
-    password: users.password
-  }
+  // $scope.profile = {
+  //   username: users.username,
+  //   password: users.password
+  // };
 });
