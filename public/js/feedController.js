65e255e50d828b238cfa00441867cb97b5986db9
Avi Samloff <avi.samloff@gmail.com>
2015-11-17 20:46:58 -0800

    (bugfix) refactor codebase

--- public/js/feedController.js
+++ public/js/feedController.js
@@ -21,27 +21,4 @@ angular.module('tastebuds', [])
 
   });
 
-
-  // .controller('profileController', function($scope, $http) {
-  //     $scope.profile = {
-  //         username: users.username,
-  //         posts: [],
-  //         wantToEat: []
-  //     };
-  //     $scope.getPosts = $http.get({
-  //         method: 'GET',
-  //         url: '/posts'
-  //     }).then(function(posts) {
-  //         return posts;
-  //     });
-  //     $scope.getEats = $http.get({
-  //         method: 'GET',
-  //         // can this url be directed at the getEats row in the posts table?
-  //         url: '/posts'
-  //     }).then(function(eat) {
-  //         wantToEat.push(eat);
-  //     });
-  // });
-
-
 });
