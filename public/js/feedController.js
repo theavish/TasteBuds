df7e5654818128b84b0d2a930756561d8f3f08c2
VDUCHEW <vidiuchiu@gmail.com>
2015-11-17 18:12:59 -0800

    (fix)adjust index and feed template

--- public/js/feedController.js
+++ public/js/feedController.js
@@ -6,15 +6,42 @@ angular.module('tastebuds', [])
     url: '/posts'
   }).then(function(response) {
     // when the response is available
-    if (response) {
+    if (response.image) {
       console.log($scope.image);
-      $scope.image = response.data;
+      $scope.image = response.data.image;
     }
+
   }, function errorCallback(error) {
     // or server returns response with an error status.
     //run error
     if (error) {
       console.log('feedController error!');
     }
+
+
   });
+
+
+  // .controller('profileController', function($scope, $http) {
+  //     $scope.profile = {
+  //         username: users.username,
+  //         posts: [],
+  //         wantToEat: []
+  //     };
+  //     $scope.getPosts = $http.get({
+  //         method: 'GET',
+  //         url: '/posts'
+  //     }).then(function(posts) {
+  //         return posts;
+  //     });
+  //     $scope.getEats = $http.get({
+  //         method: 'GET',
+  //         // can this url be directed at the getEats row in the posts table?
+  //         url: '/posts'
+  //     }).then(function(eat) {
+  //         wantToEat.push(eat);
+  //     });
+  // });
+
+
 });
